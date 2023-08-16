42cdf487ac195a581b9d945e9e33c04666256e51
PauloScotti <paulo.scotti@hotmail.com>
2023-08-16 18:11:09 -0300

    incluindo delete na politica de cors

--- middlewares/politicaCors.ts
+++ middlewares/politicaCors.ts
@@ -2,22 +2,22 @@ import type { NextApiRequest, NextApiResponse, NextApiHandler } from "next";
 import type { RespostaPadraoMsg } from "../types/RespostaPadraoMsg";
 import NextCors from "nextjs-cors";
 
-export const politicaCors = (handler : NextApiHandler) => 
-    async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
+export const politicaCors = (handler: NextApiHandler) =>
+    async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
 
-        try{
+        try {
 
             await NextCors(req, res, {
-                origin : '*',
-                methods : ['GET', 'POST', 'PUT'],
-                optionSuccessStatus : 200, // navegadores antigos dão problemas se retorna 204 (No Content) sem retorno
+                origin: '*',
+                methods: ['GET', 'POST', 'PUT', 'DELETE'],
+                optionSuccessStatus: 200, // navegadores antigos dão problemas se retorna 204 (No Content) sem retorno
             });
 
             return handler(req, res);
 
-        }catch(e){
+        } catch (e) {
             console.log('Erro ao tratar a política de CORS', e);
-            return res.status(500).json({erro : 'Ocorreu erro ao tratar a política de CORS'});
+            return res.status(500).json({ erro: 'Ocorreu erro ao tratar a política de CORS' });
         }
 
     }
